2040561a08c3369f942699622cf6ea90b0c5637c
Kristjan Rang <mail@kristjanrang.eu>
2012-07-23 00:50:06 +0300

    Revert progressive enhancement for javascript disabled users. I'll only be admitting users who have javascript enabled. Add user upgrade task.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,6 @@ class MessagesController < ApplicationController
   respond_to :html, :json
 
   def index
-    @message = Message.new
     @messages = @messages.includes(:user)
 
     if params[:page]
@@ -17,16 +16,12 @@ class MessagesController < ApplicationController
 
   def show
     #@message = Message.find(params[:id])
-    respond_with @message do |format|
-      format.html { redirect_to root_path }
-    end
+    respond_with @message
   end
 
   def new
     #@message = Message.new
-    respond_with @message do |format|
-      format.html { redirect_to root_path }
-    end
+    respond_with @message
   end
 
   def edit
@@ -38,25 +33,19 @@ class MessagesController < ApplicationController
     @message.user = current_user
     @message.save
 
-    respond_with @message do |format|
-      format.html { redirect_to root_path }
-    end
+    respond_with @message
   end
 
   def update
     #@message = Message.find(params[:id])
     @message.update_attributes(params[:message])
-    respond_with @message do |format|
-      format.html { redirect_to root_path }
-    end
+    respond_with @message
   end
 
   def destroy
     #@message = Message.find(params[:id])
     @message.destroy
     #head :ok
-    respond_with @message do |format|
-      format.html { redirect_to root_path }
-    end
+    respond_with @message
   end
 end
--- lib/tasks/maintenance.rake
+++ lib/tasks/maintenance.rake
@@ -1,10 +1,23 @@
-desc "Reprocess all messages to update html"
-task :reprocess => :environment do
-  Mongoid.unit_of_work(disable: :all) do
-    Message.all.each do |m|
-      puts "Processing " + m.id.to_s
-      m.text = m.text
-      m.save
+namespace :messages do
+  desc "Reprocess all messages to update html"
+  task :reprocess => :environment do
+    Mongoid.unit_of_work(disable: :all) do
+      Message.all.each do |m|
+        puts "Processing " + m.id.to_s
+        m.text = m.text
+        m.save
+      end
+    end
+  end
+end
+
+namespace :users do
+  desc "Make sure all users have necessary generated values"
+  task :upgrade => :environment do
+    Mongoid.unit_of_work(disable: :all) do
+      User.all.each do |m|
+        user.upgrade_user
+      end
     end
   end
 end
