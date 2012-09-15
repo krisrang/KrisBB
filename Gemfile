5bf9328ba29daa9ec167842bcc72bc5aa5d1c9a2
Kristjan Rang <mail@kristjanrang.eu>
2012-09-15 21:29:51 +0300

    Add pusher. Remove railsonfire.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby "1.9.3"
 gem 'rails', '3.2.8'
 gem 'foreman'
 gem 'puma'
+gem 'pusher'
 
 # Persistence & caching
 gem 'mongoid'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,5 @@
+require 'pusher'
+
 Kreubb::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
@@ -31,3 +33,7 @@ Kreubb::Application.configure do
   config.action_mailer.default_url_options = { :host => 'kreubb.dev' }
   config.mongoid.preload_models = true
 end
+
+Pusher.app_id = '27770'
+Pusher.key    = 'bb6ee2e971248c187d7c'
+Pusher.secret = 'a33c19e597ab7d5eba67'
