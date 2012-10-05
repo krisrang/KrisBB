22901d5fe9a2ccc4aba0912d89fc4b6f746f9343
Kristjan Rang <mail@kristjanrang.eu>
2012-10-05 09:41:19 +0300

    CarrierWave config for TDDIUM

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,24 +1,27 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['S3_KEY'],
-    :aws_secret_access_key  => ENV['S3_SECRET'],
-    :region                 => 'eu-west-1'
-  }
+  if Rails.env.production? || Rails.env.development?
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      :region                 => 'eu-west-1'
+    }
 
-  one_year = 31557600
-  config.fog_directory  = 'krisbb-assets'
-  config.fog_host       = Settings.assethost.fog
-  config.fog_public     = true
-  config.fog_attributes = {cache_control: "public, max-age=#{one_year}",
-                           expires: CGI.rfc1123_date(Time.now + one_year)}
-
-  config.delete_tmp_file_after_storage = false if Rails.env.development?
-end
-
-if Rails.env.test? or Rails.env.cucumber?
-  CarrierWave.configure do |config|
-    config.storage = :file
-    config.enable_processing = false
+    one_year = 31557600
+    config.fog_directory  = 'krisbb-assets'
+    config.fog_host       = Settings.assethost.fog
+    config.fog_public     = true
+    config.fog_attributes = {cache_control: "public, max-age=#{one_year}",
+                             expires: CGI.rfc1123_date(Time.now + one_year)}
+  elsif Rails.env.test?
+    if ENV['TDDIUM']
+      config.storage = :file
+      config.root = Dir.tmpdir
+      config.cache_dir = "carrierwave"
+    else
+      config.storage = :file
+      config.delete_tmp_file_after_storage = false
+      config.enable_processing = false
+    end
   end
 end
