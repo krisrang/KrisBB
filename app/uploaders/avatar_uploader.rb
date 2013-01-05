963bc6af6dd30e4ded1361880bfb18baa4c4c8da
Kristjan Rang <mail@kristjanrang.eu>
2013-01-05 21:42:05 +0200

    Set up carrierwave for S3

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -8,7 +8,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  storage :fog
 
   process quality: 85
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,7 +1,21 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.asset_host = Settings.assethost.production
+    config.storage :fog
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      :region                 => 'us-east-1'
+    }
+
+    one_year = 31557600
+    config.asset_host     = Settings.assethost.production
+    config.fog_directory  = 'kreu-krisbb-assets'
+    config.fog_public     = true
+    config.fog_attributes = {cache_control: "public, max-age=#{one_year}",
+                             expires: CGI.rfc1123_date(Time.now + one_year)}
   elsif Rails.env.test? && ENV['TDDIUM']
+    config.storage = :file
     config.root = Dir.tmpdir
     config.cache_dir = "carrierwave"
   end
