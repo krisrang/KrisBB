97ace93d5d46e0d737c7a730f6f3b8bf63753690
Kristjan Rang <mail@rang.ee>
2013-12-03 14:32:20 +0200

    Revert "Skip broken quality changer on uploader"
    This reverts commit 49f64246d073f01767c467c12fb5acb3ece2a3fe.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -10,7 +10,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :fog
 
-  # process quality: 85
+  process quality: 85
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -20,3 +20,15 @@ CarrierWave.configure do |config|
     config.cache_dir = "carrierwave"
   end
 end
+
+module CarrierWave
+  module MiniMagick
+    def quality(percentage)
+      manipulate! do |img|
+        img.quality(percentage.to_s)
+        img = yield(img) if block_given?
+        img
+      end
+    end
+  end
+end
